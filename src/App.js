0737f25ab99efe95c7f74c07373bf8052bfffeb8
Prince Radebe <princeradebe@gmail.com>
2023-02-13 16:59:03 +0200

    Project Setup, TailwindCSS and React

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App flex items-center justify-center ">
+      <h1 className="text-lg text-red-600 font-bold">SET UP DONE</h1>
+    </div>
+  );
+}
+
+export default App;
